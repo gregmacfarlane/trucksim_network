c533cdf4d77151c9604b29dac401e932e3ce52eb
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-05-21 11:08:36 -0400

    Moved downloaded data files into their own folder.

--- build_network.sh
+++ build_network.sh
@@ -4,16 +4,17 @@
 # Pull osm xml data file using the Overpass API.
 echo "Downloading OSM Freight Network "
 # highway network
-wget -O nc_freeways.osm \
+wget -O data/nc_freeways.osm \
 	--post-file=queries/query_northcarolina.xml \
 	"http://overpass-api.de/api/interpreter"
 
 
 # trim to usa polyline boundary (if USA)
-osmosis --read-xml usa_freeways.osm\
+osmosis --read-xml data/usa_freeways.osm\
  --bp file=./query/usa.poly\
- --write-xml .usa_freeways.osm
+ --write-xml data/usa_freeways.osm
 
 
 # 2 ------------------
 echo "Converting to MATSim network"
+
