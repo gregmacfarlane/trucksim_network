b42aeac0bb876ed7cc879575c1ea4e0e2e142e14
Greg Macfarlane <macfarlaneg@pbworld.com>
2015-06-02 11:11:25 -0400

    Get national highway system from FWA in geojson format.

--- build_network.sh
+++ build_network.sh
@@ -3,35 +3,16 @@
 matsim_path="/home/greg/Desktop/matsim-0.6.0/*"
 
 # 1 ----------------
-# Pull osm xml data file using the Overpass API. 
-# Need to get the data in pieces or API will reject
-echo "Downloading OSM Freight Network "
-for i in northeast south midwest west; do
-  echo 
-  ## get the osm data
-  wget -O data/usa_${i}_freeways.osm --timeout=0 \
-   --post-file=queries/usa_${i}.xml "http://overpass-api.de/api/interpreter"
+# Download national highway system GeoJSON file and unzip
+wget -o data/nhs.zip \
+ "http://www.fhwa.dot.gov/planning/processes/tools/nhpn/2015/nhpnv14-05geojson.zip"
 
-  ## convert to the most recent version of osm; why the api can't do this is 
-  ## entirely beyond me.
-  osmconvert data/usa_${i}_freeways.osm \
-   --fake-author --fake-version -o=data/usa_${i}_freeways_conv.osm
-done
+unzip data/nhs.zip -d data/
 
 
-# Post-processing of downloaded data 
 
-osmosis \
- --read-xml-0.5 data/usa_northeast_freeways.osm \
- --read-xml-0.5 data/usa_south_freeways.osm \
- --merge \
- --write-xml data/usa_east_freeways.osm
-
- --read-xml data/usa_midwest_freeways.osm \
- --read-xml data/usa_west_freeways.osm \
- --bp file=./query/usa.poly \
-
-
+# 2 ----------------
+# Convert GeoJSON to osm.xml
 
 # 2 ------------------
 echo "Converting to MATSim network"
